41f921f0e10f4a074cf86115382b85b16dffe022
armedturret <rjwhitenh@outlook.com>
2026-01-23 14:39:48 -0500

    Initial files

---
+++ raytracer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    glm::vec4 my_vec = glm::vec4(1.0);
+    cout << my_vec.x << endl;
+}
