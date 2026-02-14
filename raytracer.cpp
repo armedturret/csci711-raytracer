6d126821e7fc13af812575c415780a908bea85aa
Robert White <rjwhitenh@outlook.com>
2026-02-13 22:53:19 -0500

    Blew up the rabbit

--- raytracer.cpp
+++ raytracer.cpp
@@ -30,7 +30,6 @@ int main()
 
     PhongMaterial planeMat(yellow, white, 0.5f, 0.5f, 1.0f);
     PhongMaterial sphereMat(glm::vec3(0.361f), white, 0.5f, 0.5f, 5.0f);
-    PhongMaterial finkMat(white, white, 0.7f, 0.3f, 1.0f);
 
     // Objects
     vector<glm::vec3> plane = {
@@ -47,22 +46,11 @@ int main()
     Sphere s2(&sphereMat,
         glm::vec3(1.801f, 0.537f, -1.423f),
         0.5f);
-    // TODO: Uncomment
-    //w.add(&s1);
-    //w.add(&s2);
+    w.add(&s1);
+    w.add(&s2);
 
     Mesh m(&planeMat, glm::scale(glm::mat4(1.0f), glm::vec3(4.0f)), plane);
     w.add(&m);
-    glm::mat4 finkT(1.0f);
-    finkT = glm::translate(finkT, glm::vec3(1.014f, 0.0f, -1.5f));
-    finkT = glm::scale(finkT, glm::vec3(0.01f));
-    Mesh fink(&finkMat, finkT, "finklestein.fbx");
-    w.add(&fink);
-    glm::mat4 rabbitT(1.0f);
-    rabbitT = glm::translate(rabbitT, glm::vec3(1.75f, 0.0f, -1.0f));
-    rabbitT = glm::scale(rabbitT, glm::vec3(5.0f));
-    Mesh rabbit(&finkMat, rabbitT, "bun_zipper.ply");
-    //w.add(&rabbit);
 
     // Build KD Tree before rendering
     w.buildKdTree(20, 36);
@@ -74,7 +62,7 @@ int main()
         glm::vec3(0.0f, 0.796f, 0.82f),
         0.15f,
         0.1f);
-    c.render(w, "test_render.png", 960 / 4, 540 / 4, false, 1);
+    c.render(w, "test_render.png", 960, 540, true, 1);
 
     cout << "Press a key to continue..." << endl;
     string a;
